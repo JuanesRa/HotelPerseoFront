16b632694008a9ef0c39115ad21ee3a2de76b0bd
JuanesRa <juanes.ro2002@gmail.com>
2023-03-07 11:50:04 -0500

    Envio index con el header completo

---
+++ Principal.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor_izq">
+            <div class="caja_izq">
+                <img src="img/escudo_barca.png" alt="">
+            </div>
+        </div>
+        <div class="contenedor_medio">
+            <div class="caja_mid">
+                <img src="img/FCB.jpg" alt="">
+            </div>
+        </div>
+        <div class="contenedor_der">
+            <div class="caja_der">
+                <img src="img/Spotify.jpg" alt="">
+            </div>
+        </div>
+    </header>
+    <nav></nav>
+    <section></section>
+    <aside></aside>
+    <footer></footer>
+    
+</body>
+</html>
