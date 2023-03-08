e2b5a755528fdfe4d4730854e53ad2622d11b569
JuanesRa <juanes.ro2002@gmail.com>
2023-03-08 07:48:19 -0500

    Se copia el index de html a principal php

--- Principal.php
+++ Principal.php
@@ -5,30 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+    <script src="js/script.js" defer></script>
+    <title>Hotel Perseo</title>
 </head>
 <body>
     <header>
-        <div class="contenedor_izq">
-            <div class="caja_izq">
-                <img src="img/escudo_barca.png" alt="">
-            </div>
+        <div class="logo_header">
+                <a href="index.html"><img src="img/LogoPerseo.png" alt=""></a>
         </div>
-        <div class="contenedor_medio">
-            <div class="caja_mid">
-                <img src="img/FCB.jpg" alt="">
+
+        <nav>
+            <div class="nav-content">
+              <ul class="nav-links">
+                <li><a href="#" target="">RESERVAR</a></li>
+                <li><a href="#" target="">OFERTAS</a></li>
+                <li><a href="#" target="">NUESTRAS MARCAS</a></li>
+                <li><a href="#" target="">ACERCA DE NOSOTROS</a></li>
+              </ul>
             </div>
-        </div>
-        <div class="contenedor_der">
-            <div class="caja_der">
-                <img src="img/Spotify.jpg" alt="">
-            </div>
-        </div>
+        </nav>
+
     </header>
-    <nav></nav>
-    <section></section>
-    <aside></aside>
-    <footer></footer>
     
 </body>
 </html>
